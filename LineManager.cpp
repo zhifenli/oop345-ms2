9d058d758b8dd5c76e26e84c0dffa33aada64de1
fen <zhifenli1121@gmail.com>
2023-04-05 23:37:36 -0400

    good

--- LineManager.cpp
+++ LineManager.cpp
@@ -5,95 +5,85 @@ namespace sdds
     LineManager::LineManager(const std::string &file, const std::vector<Workstation *> &stations)
     {
         Utilities ut;
-        bool more = true;
 
-        ifstream assmblyLineFile(file);
-        if (!assmblyLineFile)
+        ifstream fileStream(file);
+        if (!fileStream)
         {
-            throw "Unable to open [" + file + "] file.";
+            throw "Unable to open " + file + " file.";
         }
         else
         {
-            string record;
-            while (std::getline(assmblyLineFile, record))
+            string line;
+            while (std::getline(fileStream, line))
             {
                 size_t next_pos = 0;
 
-                Workstation *ws{};
-                std::string station = ut.extractToken(record, next_pos, more);
+                bool more = false;
+                std::string stationName1 = ut.extractToken(line, next_pos, more);
+                Workstation *station1 = nullptr;
+                for_each(stations.begin(), stations.end(), [&stationName1, &station1](Workstation *ws)
+                         { if (ws->getItemName() == stationName1) { station1 = ws; } });
+
+                m_activeLine.push_back(station1);
+
+                Workstation *station2 = nullptr;
+
                 if (more)
                 {
-                    std::string next_station = ut.extractToken(record, next_pos, more);
+                    // this line has 2 stations
+                    std::string stationName2 = ut.extractToken(line, next_pos, more);
 
-                    for_each(stations.begin(), stations.end(), [&station, &ws](Workstation *wstemp)
-                             { if (wstemp->getItemName() == station) { ws = wstemp; } });
-                    for_each(stations.begin(), stations.end(), [&next_station, &ws](Workstation *nstemp)
-                             { if (nstemp->getItemName() == next_station) { ws->setNextStation(nstemp); } });
-                    m_activeLine.push_back(ws);
+                    for_each(stations.begin(), stations.end(), [&stationName2, &station2](Workstation *ws)
+                             { if (ws->getItemName() == stationName2) { station2 = ws; } });
                 }
-                else
-                {
 
-                    for_each(stations.begin(), stations.end(), [&station, &ws](Workstation *wstemp)
-                             {
-                                 if (wstemp->getItemName() == station)
-                                 {
-                                     ws = wstemp;
-                                     ws->setNextStation(nullptr);
-                                 } });
-                    m_activeLine.push_back(ws);
-                }
+                station1->setNextStation(station2);
             }
         }
 
-        // cout << "######## constructor m_activeLine: ########" << endl;
-        // for_each(m_activeLine.begin(), m_activeLine.end(), [](Workstation *ws)
-        //          { cout << ws->getItemName() << "," << endl; });
-        // cout << "######## constructor m_activeLine endl ########" << endl;
-
-        assmblyLineFile.close();
+        fileStream.close();
         m_cntCustomerOrder = g_pending.size();
 
-        // find the first station
+        std::vector<Workstation *> activeLineCopy = m_activeLine;
+        Workstation *temp_firstStation = nullptr;
 
-        std::vector<Workstation *> temp_activeLine = m_activeLine;
-        Workstation *temp_firstStation{};
-
-        for_each(m_activeLine.begin(), m_activeLine.end(), [&temp_activeLine, &temp_firstStation](Workstation *ws)
+        for_each(m_activeLine.begin(), m_activeLine.end(),
+                 [&activeLineCopy, &temp_firstStation](Workstation *possibleFirst)
                  {
-                     bool found{};
+                     bool isActualFirst = true;
 
-                     for_each(temp_activeLine.begin(), temp_activeLine.end(), [&ws, &found, &temp_firstStation](Workstation *ns)
+                     for_each(activeLineCopy.begin(), activeLineCopy.end(),
+                              [&possibleFirst, &isActualFirst, &temp_firstStation](Workstation *other)
                               {
+                                  if (other->getNextStation() && other->getNextStation()->getItemName() == possibleFirst->getItemName())
+                                  {
+                                      isActualFirst = false;
+                                  }
+                              });
 
-                  if (ns->getNextStation() && ws->getItemName() == ns->getNextStation()->getItemName()) {
-
-                     found = true;
-                  } });
-                     if (!found && ws->getNextStation() != nullptr)
+                     if (isActualFirst)
                      {
-                         temp_firstStation = ws;
-                     } });
+                         if (temp_firstStation)
+                         {
+                             throw std::string("Multiple first station found");
+                         }
+                         temp_firstStation = possibleFirst;
+                     }
+                 });
         m_firstStation = temp_firstStation;
     }
 
     void LineManager::reorderStations()
     {
-
-        std::vector<Workstation *> temp;
-        temp.push_back(m_firstStation);
-        Workstation *current_station = m_firstStation;
-        while (current_station->getNextStation())
+        std::vector<Workstation *> activeline;
+        Workstation *cur = m_firstStation;
+        while (cur)
         {
-            current_station = current_station->getNextStation();
-            temp.push_back(current_station);
+            activeline.push_back(cur);
+            cur = cur->getNextStation();
         }
-        m_activeLine = temp;
 
-        // cout << "######## reorderStations m_activeLine: ########" << endl;
-        // for_each(m_activeLine.begin(), m_activeLine.end(), [](Workstation *ws)
-        //          { cout << ws->getItemName() << "," << endl; });
-        // cout << "######## reorderStations m_activeLine endl ########" << endl;
+        m_activeLine = activeline;
     }
 
     bool LineManager::run(std::ostream &os)
@@ -103,23 +93,28 @@ namespace sdds
         ++count;
         os << "Line Manager Iteration: " << count << endl;
 
-        if (g_pending.size() > 0)
+        if (!g_pending.empty())
         {
             *m_firstStation += std::move(g_pending.front());
             g_pending.pop_front();
         }
 
-        for_each(m_activeLine.begin(), m_activeLine.end(), [&os](Workstation *wsptr)
-                 { wsptr->fill(os); });
+        for_each(m_activeLine.begin(), m_activeLine.end(),
+                 [&os](Workstation *ws)
+                 {
+                     ws->fill(os);
+                 });
 
-        for_each(m_activeLine.begin(), m_activeLine.end(), [](Workstation *wsptr)
-                 { wsptr->attemptToMoveOrder(); });
+        for_each(m_activeLine.begin(), m_activeLine.end(),
+                 [](Workstation *ws)
+                 {
+                     ws->attemptToMoveOrder();
+                 });
 
         if (g_completed.size() + g_incomplete.size() == m_cntCustomerOrder)
         {
             ret = true;
         }
-
         return ret;
     }
 
